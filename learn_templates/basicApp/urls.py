0e7ad82e16ab75c8054075cc0cef150df60505cb
inaki <inakifernandez@hotmail.com>
2021-08-25 10:45:29 -0300

    first commit

---
+++ learn_templates/basicApp/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from basicApp import views #from . import views
+
+#template tagging
+app_name = 'basicApp'
+
+urlpatterns = [
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other'),
+]
