9cdd694848e327ed7a9419a749dcbd2d3935a4ea
Meirill <88006444+Meirill@users.noreply.github.com>
2021-09-03 11:47:23 -0300

    Update settings.py

--- learn_templates/learn_templates/settings.py
+++ learn_templates/learn_templates/settings.py
@@ -21,7 +21,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xxm$@oo2jrq&ha@18q7dh@h_ffadec_*xa6$-a3!^8a-e+_^)z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
